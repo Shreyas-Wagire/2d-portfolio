673526f528de9c38fcf82dceb64c218152edfe9f
Shreyas Wagire <shreyaswagire07sw@gmail.com>
2024-11-09 15:36:02 +0530

    new

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Web Development these days. I also like C++ and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/shreyas-wagire" target="_blank">Github</a>!`,
+  "cs-degree": `This is my first winning certificate. I hung it on the wall because I'm proud of it. It was a web development certificate.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also solve a lot of problems here.  <a href="/" target="_blank">my channel</a>!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@CodeWithHarry" target="_blank">Code with Harry</a>, <a href="https://www.youtube.com/@ApnaCollegeOfficial" target="_blank">Apna College</a>,
+  <a href="https://www.youtube.com/@javascriptmastery" target="_blank">javascript mastery</a> and <a href="https://www.youtube.com/@comicverseog" target="_blank">Comic Verse</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1C7vmceMmNT9IKUGjYx6VfSQcfCUejioW/view?usp=sharing" target="_blank">Check it out?</a>
+  Contact me at shreyaswagire07sw@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of projects on my shelves.`,
+  exit: `If you want to exit SW's portfolio, just close the tab.`,
+};
